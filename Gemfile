6c1fbc27b2b39d6971e0afc670df796003655887
Feifan Wang <feifanw@gmail.com>
2017-01-28 16:35:20 -0600

    set up deployment

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source 'https://rubygems.org'
 # Middleman Gems
 gem 'middleman', '>= 4.0.0'
 gem 'middleman-livereload'
+gem 'middleman-deploy', '= 2.0.0.pre.alpha'
 
 gem 'slim'
--- config.rb
+++ config.rb
@@ -45,3 +45,8 @@ configure :build do
   # Minify Javascript on build
   # activate :minify_javascript
 end
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.deploy_method = :git
+end
