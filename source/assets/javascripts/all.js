64bee88064d456187bb9fc4e57c4fa26e7ddf189
Feifan Wang <feifanw@gmail.com>
2017-01-30 01:13:21 -0600

    add typed.js for a fancy hero panel

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,1 +1,20 @@
-// This is where it all goes :)
+$(document).ready(() => {
+  $(".typed").typed({
+    stringsElement: $('.typed-words'),
+    typeSpeed: 85,
+    // time before typing starts
+    startDelay: 60,
+    // backspacing speed
+    backSpeed: 10,
+    // time before backspacing
+    backDelay: 1500,
+    cursorChar: '_',
+    // loop: true,
+    // callback: () => {
+    //   window.setTimeout(() => {
+    //     $('.typed-cursor').hide();
+    //   }, 1000);
+    // }
+  });
+});
+
